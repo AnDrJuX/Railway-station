66c8ee13f0d3da9aeb97b7ce8b4b752642e4a13c
Andres Peterson <andres_p@mail.ru>
2017-08-23 19:22:34 +0300

    add to trains views & controller

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170823154513) do
+ActiveRecord::Schema.define(version: 20170823161122) do
 
   create_table "premium_wagons", force: :cascade do |t|
     t.integer "number"
@@ -26,6 +26,8 @@ ActiveRecord::Schema.define(version: 20170823154513) do
     t.integer "railway_station_id"
     t.integer "route_id"
     t.integer "position"
+    t.datetime "arrival"
+    t.datetime "departure"
   end
 
   create_table "routes", force: :cascade do |t|
