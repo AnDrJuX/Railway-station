b43770f2409471ca87035fda6b7be0b63381a96c
Andres Peterson <andres_p@mail.ru>
2017-08-29 19:24:36 +0300

    исправил ошибку 17 ого задания))

--- config/routes.rb
+++ config/routes.rb
@@ -20,6 +20,7 @@ Rails.application.routes.draw do
   end
 
   get 'welcome/index'
+  #root 'searches#show'
 
   root 'welcome#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
