2bd60db0783663fff8e4096e17dc9a99ab8eaa42
Andres Peterson <andres_p@mail.ru>
2017-09-17 12:16:04 +0300

    add chenge to controllers/admin to update actions

--- app/controllers/admin/railway_stations_controller.rb
+++ app/controllers/admin/railway_stations_controller.rb
@@ -33,7 +33,7 @@ class Admin::RailwayStationsController < Admin::BaseController
   # PATCH/PUT /railway_stations/1
   def update
     if @railway_station.update(railway_station_params)
-      redirect_to [:admin, @railway_station]
+      redirect_to admin_railway_stations_path
     else
       render :edit
     end
